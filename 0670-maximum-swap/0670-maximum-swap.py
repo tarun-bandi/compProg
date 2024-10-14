7e9236dc29971fdb858d8143b56e3a822137f42b
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2024-10-14 01:30:54 -0400

    Time: 34 ms (61.04%), Space: 16.3 MB (97.22%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,17 +1,22 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
+        
+        swap_1 = -1
+        swap_2 = -1
+        largest = -1
         num = list(str(num))
-        ordered = sorted(num, reverse=True)
-        unEq = 0
-        for i in range(len(num)):
-            if num[i] != ordered[i]:
-                for j in range(len(num) - 1, i - 1, -1):
-                    if num[j] == ordered[i]:
-                        unEq = j
-                        break
-                num[i], num[j] = num[j], num[i]
-                return int("".join(num))
+        for i in range(len(num) - 1, -1 ,-1):
+            if largest == -1 or num[i] > num[largest]:
+                largest = i
+            elif num[i] < num[largest]:
+                swap_1 = i
+                swap_2 = largest
+        
+        num[swap_1], num[swap_2] = num[swap_2], num[swap_1]
         return int("".join(num))
+
+            
+
 
 
 
