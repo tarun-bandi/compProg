1e3905e0502fda20050e1171f614f60a2b909679
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-04-07 15:12:54 -0400

    Time: 92 ms (47.46%), Space: 61 MB (44.48%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -5,7 +5,17 @@ class Solution:
         #Dynamic programming problem - consider every choice and take the maximum
 
         #Pick/don't pick
+        dp = [0 for _ in range(len(questions))]
 
+        for i in range(len(questions) - 1, -1, -1):
+            points, power = questions[i]
+            if i + power + 1 >= len(questions):
+                dp[i] = points
+            else:
+                dp[i] = points + dp[i + power + 1]
+            if i < len(questions) - 1:
+                dp[i] = max(dp[i + 1], dp[i])
+        return dp[0]
         @cache
         def dp(i : int) -> int:
             if i >= len(questions): return 0
