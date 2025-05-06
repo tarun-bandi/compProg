83914726f0340595cb7797c5dcb71c468ddf5d30
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-05-06 11:06:55 -0400

    Time: 0 ms (100%), Space: 12.7 MB (32.77%) - LeetHub

---
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [nums[i] for i in nums]
+
+        
