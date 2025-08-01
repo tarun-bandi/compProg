ab575277fd913c2e31e64c38b406966ceac34d61
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-01 09:57:55 -0700

    Time: 34 ms (23.21%), Space: 18.2 MB (64.95%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,21 +1,38 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        seen = set()
+        visited = set()
+        def bfs(r : int, c : int): 
+            queue = deque()
+
+            def is_invalid(r : int, c : int) -> bool:
+                return (r, c) in visited or r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == 0
+                    
+
+            queue.append((r, c))
+            dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+            area = 0
+            while queue:
+                (r, c) = queue.popleft()
+                if (r, c) in visited:
+                    continue
+                visited.add((r, c))
+                area += 1
+                for dx, dy in dirs:
+                    x, y = r + dx, c + dy
+                    if not is_invalid(x, y):
+                        queue.append((x, y))
+            
+            return area
+        largest = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if (r, c) not in visited and grid[r][c] == 1:
+                    largest = max(largest, bfs(r, c))
+        
+        return largest
 
 
-        def dfs(x, y):
-            if (0 <= x < len(grid) and 0 <= y < len(grid[0]) 
-                and grid[x][y] == 1 and (x, y) not in seen):
-                seen.add((x, y))
-                res = 1
-                for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    res += dfs(x + dx, y + dy) 
-                return res
-            return 0
-        
-        cc = 0
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                cc = max(cc, dfs(x, y))
-        
-        return cc
+
+                
+            
+            
