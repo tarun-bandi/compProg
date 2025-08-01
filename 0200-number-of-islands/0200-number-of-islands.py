9dd54a1415dc193eb07449ba678977857306e89d
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-07-31 20:16:08 -0700

    Time: 335 ms (5.06%), Space: 25.2 MB (30.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,37 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        seen = set()
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        island_count = 0
+        visited = set()
+
+        def bfs(row, col):
+
+            queue = deque()
+
+            queue.append((row, col))
+            directions = [(0, 1), (-1, 0), (1, 0), (0, -1)]
+            while queue:
+                x, y = queue.popleft()
+                if (x, y) in visited:
+                    continue
+                visited.add((x, y))
+                for dx, dy in directions:
+                    new_x = x + dx
+                    new_y = y + dy
+                    if (new_x, new_y) in visited:
+                        continue
+                    if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and grid[new_x][new_y] == "1":
+                        queue.append((new_x, new_y))
+            
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                print(r, c)
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    island_count += 1
+                    bfs(r, c)
+        return island_count
 
 
-        def dfs(x, y):
-            if (0 <= x < len(grid) and 0 <= y < len(grid[0]) 
-                and grid[x][y] == "1" and (x, y) not in seen):
-                seen.add((x, y))
-                res = 1
-                for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    res += dfs(x + dx, y + dy) 
-                return res
-            return 0
+
+
         
-        cc = 0
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if dfs(x, y) > 0:
-                    cc += 1
-        
-        return cc
-                
