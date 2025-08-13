17162ce893db9e62a5ba8174ae655c248e4f813c
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-13 17:04:37 -0400

    Time: 275 ms (26.38%), Space: 25.5 MB (22.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,37 +1,55 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        island_count = 0
-        visited = set()
+        ROW_COUNT = len(grid)
+        COLUMN_COUNT = len(grid[0])
 
-        def bfs(row, col):
-
-            queue = deque()
-
-            queue.append((row, col))
-            directions = [(0, 1), (-1, 0), (1, 0), (0, -1)]
-            while queue:
-                x, y = queue.popleft()
-                if (x, y) in visited:
-                    continue
-                visited.add((x, y))
-                for dx, dy in directions:
-                    new_x = x + dx
-                    new_y = y + dy
-                    if (new_x, new_y) in visited:
-                        continue
-                    if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and grid[new_x][new_y] == "1":
-                        queue.append((new_x, new_y))
-            
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                print(r, c)
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    island_count += 1
-                    bfs(r, c)
-        return island_count
-
-
-
-
+        island_counter = 0 
+        island_cells_visited = set()
         
+        def find_all_connected_cells(r, c):
+            """
+            This uses BFS to find all connected cells, using the overall 
+            shared set for visited
+            """
+            dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+            queue = deque()
+            queue.append((r, c))
+
+            def cell_is_unexplored(row, col):
+                if row < 0 or row >= ROW_COUNT:
+                    return False
+                
+                if col < 0 or col >= COLUMN_COUNT:
+                    return False
+                
+                if grid[row][col] != "1" or (row, col) in island_cells_visited:
+                    return False
+                
+                return True
+            
+
+            while queue:
+                row, col = queue.popleft()
+
+                if (row, col) in island_cells_visited:
+                    continue
+
+                island_cells_visited.add((row, col))
+                
+                # Look in all four directions
+                for dx, dy in dirs:
+                    if cell_is_unexplored(row + dx, col + dy):
+                        queue.append((row + dx, col + dy))
+
+            return
+
+        for r in range(ROW_COUNT):
+            for c in range(COLUMN_COUNT):
+                if grid[r][c] == "1" and (r, c) not in island_cells_visited:
+                        find_all_connected_cells(r, c)
+                        island_counter += 1
+        
+        return island_counter
