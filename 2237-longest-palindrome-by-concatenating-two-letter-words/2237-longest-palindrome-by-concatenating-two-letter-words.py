9f72941a14a9cd731b06b764fc7977fe2a7b3a03
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-17 11:52:58 -0400

    Time: 213 ms (12.13%), Space: 35.5 MB (94.23%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,34 +1,23 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-
-        """
-        Observations:
-        We want to find "Pairs" - essentially (cl, lc) are a pair. We also want to find "middle" elements which are the same 2 characters
-        """
-
-        # We only can include one middle element
-
-        middle_element_found = False
-        seen = dict()
-        pairs = 0 
-        words.sort()
-        print(words)
+        
+        word_counter = collections.defaultdict(int)
+        palindome_size = 0
         for word in words:
-            
-            if word not in seen or seen[word] == 0:
-                seen[word[::-1]] = seen.get(word[::-1], 0) +  1
+
+            if word[::-1] in word_counter:
+                word_counter[word[::-1]] -= 1
+
+                if word_counter[word[::-1]] == 0:
+                    del word_counter[word[::-1]]
+                palindome_size += 4
             else:
-                print(f"Pair: {word} and {word[::-1]}")
-                seen[word] -= 1
-                pairs += 4
-            
-        for word in seen:
-            if seen[word] > 0 and word[0] == word[1]:
-                middle_element_found = True
+                word_counter[word] += 1
+        
+        for word in word_counter:
+            if word[0] == word[1]:
+                palindome_size += 2
                 break
         
-        return pairs + (2 if middle_element_found else 0)
+        return palindome_size
 
-            
-
-        
