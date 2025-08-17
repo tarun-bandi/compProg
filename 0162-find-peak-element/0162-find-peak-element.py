1fbb5011d5964031c067d68639fa3f6451225310
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-17 16:49:50 -0400

    Time: 0 ms (100%), Space: 18 MB (18.57%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,28 +3,15 @@ class Solution:
 
         lo = 0
         hi = len(nums) - 1
-        if len(nums) == 1:
-            return 0
-
-        def is_peak(index: int):
-            if index == 0 and nums[0] > nums[1]:
-                return True
-            
-            if index == len(nums) - 1 and nums[index] > nums[index - 1]:
-                return True
-            
-            if nums[index] > nums[index - 1] and nums[index] > nums[index + 1]:
-                return True
-            
-            return False
         
-        while lo < hi:
+        while lo <= hi:
             mid = (lo + hi) // 2
             
-            if nums[mid] < nums[mid + 1]:
+            if mid != len(nums) - 1 and nums[mid] < nums[mid + 1]:
                 lo = mid + 1
+            elif mid != 0 and nums[mid] < nums[mid - 1]:
+                hi = mid - 1
             else:
-                hi = mid
-        return lo
+                return mid
 
         
