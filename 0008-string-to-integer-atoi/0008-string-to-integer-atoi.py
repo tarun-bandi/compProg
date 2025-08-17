cd99bba95c6cc24c0c658167ce0fedc6a20caec1
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-17 16:01:35 -0400

    Time: 0 ms (100%), Space: 17.6 MB (95.31%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,40 +1,6 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-
-        if s == "":
-            return 0
-        index = 0
-
-        while index < len(s) and s[index] == " ":
-            index += 1
-        if index == len(s):
-            return 0
-        negative = False
-        if s[index] == "-":
-            negative = True
-            index += 1
-        elif s[index] == "+":
-            index += 1
-
-        result = 0
-        if index == len(s):
-            return 0
-            
-        def rounded(num: int) -> int:
-            if num < - 2 ** 31:
-                return -(2 ** 31)
-            elif num >= 2 ** 31 - 1:
-                return 2 ** 31 - 1
-            return num
-        
-        while index < len(s):
-            if not s[index].isdigit():
-                return rounded(result * (-1 if negative else 1))
-            
-            result *= 10
-            result += int(s[index])
-            index += 1
-        return rounded(result * (-1 if negative else 1))
-
-            
-        
+        import re
+        match = re.match(r' *([+-]?\d+)', s)
+        num = int(match.group(1)) if match else 0
+        return max(min(num, 2**31 - 1), -2**31)
