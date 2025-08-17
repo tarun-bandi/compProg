8cfb40b7c49e5506cde74efdd2e01bf646b6f078
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-17 11:40:47 -0400

    Time: 0 ms (100%), Space: 17.9 MB (33.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,15 +3,14 @@ class Solution:
         """ Use a recursive DP solution to attack problem """
         
         @cache
-        def max_money_robbed_starting_from_house(index: int) -> int:
+        def rob_from(index: int) -> int:
             """ Solves the subproblem of max money from [index, end] """
 
             if index >= len(nums):
                 return 0
             
-            rob_this_house = nums[index] + max_money_robbed_starting_from_house(index + 2)
-            dont_rob_this_house = max_money_robbed_starting_from_house(index + 1)
-
-            return max(rob_this_house, dont_rob_this_house)
+            rob = nums[index] + rob_from(index + 2)
+            dont_rob = rob_from(index + 1)
+            return max(rob, dont_rob)
         
-        return max_money_robbed_starting_from_house(0)
+        return rob_from(0)
