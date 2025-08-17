e82a869192bd26672c52a30019594fa7750ad53e
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-17 16:19:34 -0400

    Time: 1155 ms (5.02%), Space: 32.1 MB (24.54%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,23 +1,22 @@
+from sortedcontainers import SortedDict
+
+
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        min_heap = []
-        max_heap = []
+        window = SortedDict()
+        left = 0
+        max_length = 0
 
-        l = 0
-        max_len = 0
-        for r in range(len(nums)):
-            heapq.heappush(max_heap, (-nums[r], r))
-            heapq.heappush(min_heap, (nums[r], r))
+        for right in range(len(nums)):
+            window[nums[right]] = window.get(nums[right], 0) + 1
 
-            while -max_heap[0][0] - min_heap[0][0] > limit:
-                l = min(max_heap[0][1], min_heap[0][1]) + 1
+            while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
+                window[nums[left]] -= 1
+                if window[nums[left]] == 0:
+                    window.pop(nums[left])
 
-                while max_heap[0][1] < l:
-                    heapq.heappop(max_heap)
-                while min_heap[0][1] < l:
-                    heapq.heappop(min_heap)
-                
-            max_len = max(max_len, r - l + 1)
-        return max_len 
+                left += 1
 
-        
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
