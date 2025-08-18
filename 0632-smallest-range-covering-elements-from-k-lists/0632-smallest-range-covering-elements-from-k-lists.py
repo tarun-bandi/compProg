1b2832aad8008d1cd0e0585d50f6468ba461f3ce
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-18 12:07:05 -0400

    Time: 280 ms (51.96%), Space: 38.9 MB (35.68%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -1,33 +1,31 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         
-        min_heap = []
-        k = len(nums)
-        
-        left = float('inf')
-        right = float('-inf')
-        
-        # initialize heap and boundaries
-        for i in range(k):
-            val = nums[i][0]
-            heapq.heappush(min_heap, (val, 0, i))
-            left = min(left, val)
-            right = max(right, val)
-        
+        minimum_elements = []
+        left = float("inf")
+        right = float("-inf")
+
+        for i, num in enumerate(nums):
+            element = num[0]
+            heapq.heappush(minimum_elements, (element, i, 0)) # insert element, list_num, position in list
+            left = min(left, element)
+            right = max(right, element)
+
         res = [left, right]
-        
         while True:
-            val, idx, list_num = heapq.heappop(min_heap)
+            _, list_num, pos = heapq.heappop(minimum_elements)
+            if pos + 1 == len(nums[list_num]):
+                return res
             
-            # update best range
-            if right - val < res[1] - res[0] or (right - val == res[1] - res[0] and val < res[0]):
-                res = [val, right]
-            
-            # move forward in the popped list
-            if idx + 1 == len(nums[list_num]):
-                break
-            new_val = nums[list_num][idx + 1]
-            heapq.heappush(min_heap, (new_val, idx + 1, list_num))
-            right = max(right, new_val)
+            pos += 1
+            new_element = nums[list_num][pos]
+            right = max(right, new_element)
+            heapq.heappush(minimum_elements, (new_element, list_num, pos))
+            left = minimum_elements[0][0]
+            if right - left < res[1] - res[0]:
+                res = [left, right]
         
-        return res
+        return -1
+
+
+            
