eada95f73623ea6b59f5fcf7ca22c725ea2ae383
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-19 00:51:17 -0400

    Time: 35 ms (80.29%), Space: 18.4 MB (56.88%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -3,42 +3,30 @@ class Solution:
 
         if len(p) > len(s):
             return []
-        
-        anagram_list = []
-        string_counts = [0] * 26
-        pattern_counts = [0] * 26
 
         def char_to_index(char: str) -> int:
             """ Uses ord to convert lowercase char to place in alphabet """
             return ord(char) - ord('a')
+
+        anagram_list = []
+        s_counts = [0] * 26
+        p_counts = [0] * 26
+
+        for index in range(len(p)):
+
+            p_counts[char_to_index(p[index])] += 1
+            s_counts[char_to_index(s[index])] += 1
         
-        def pattern_equal_counts(string_cts, pattern_cts):
-            for i in range(26):
-                if string_cts[i] != pattern_cts[i]:
-                    return False
-            return True
-
-        index = 0
-
-        while index < len(p):
-            pattern_char = p[index]
-            string_char = s[index]
-
-            pattern_counts[char_to_index(pattern_char)] += 1
-            string_counts[char_to_index(string_char)] += 1
-        
-            index += 1
         indeces = []
-        while index <= len(s):
-            print(index - len(p), pattern_counts)
-            if pattern_equal_counts(string_counts, pattern_counts):
+        for index in range(len(p), len(s)):
+            if s_counts == p_counts:
                 indeces.append(index - len(p))
-            
-            removing_character = s[index - len(p)]
-            adding_character = s[index] if index < len(s) else "a"
-            string_counts[char_to_index(removing_character)] -= 1
-            string_counts[char_to_index(adding_character)] += 1
+
+            s_counts[char_to_index(s[index - len(p)])] -= 1
+            s_counts[char_to_index(s[index])] += 1
             index += 1
+        if s_counts == p_counts:
+            indeces.append(len(s) - len(p))
         
         return indeces
 
