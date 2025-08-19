d81f095c7c2937228bdbe9b81fa2f8a0c7d46f42
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-19 10:04:35 -0400

    Time: 0 ms (100%), Space: 17.8 MB (77.74%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,6 +1,26 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        import re
-        match = re.match(r' *([+-]?\d+)', s)
-        num = int(match.group(1)) if match else 0
-        return max(min(num, 2**31 - 1), -2**31)
+
+        index = 0
+        while index < len(s) and s[index] == " ":
+            index += 1
+
+        if index == len(s):
+            return 0
+        
+        negative = False
+        if s[index] in "+-":
+            negative = s[index] == "-"
+            index += 1
+        
+        number = 0
+        while index < len(s):
+            if s[index].isdigit():
+                number *= 10
+                number += int(s[index])
+                index += 1
+            else:
+                break
+        if negative:
+            number *= -1
+        return max(min(number, 2 ** 31 - 1), - 2**31)
