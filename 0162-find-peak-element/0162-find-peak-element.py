4a4e5209d99cdcf3813eb65c493dea4624b5bf08
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-19 10:13:42 -0400

    Time: 0 ms (100%), Space: 17.8 MB (60.83%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,16 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        
+        # We want to binary search bc of the log(n) time
+        # We know we are a peak when we are bigger then the 2 elements next to us
         lo = 0
         hi = len(nums) - 1
 
         while lo <= hi:
             mid = (lo + hi) // 2
-            
             if mid != len(nums) - 1 and nums[mid] <= nums[mid + 1]:
                 lo = mid + 1
             elif mid != 0 and nums[mid] <= nums[mid - 1]:
                 hi = mid - 1
             else:
                 return mid
+        
         return -1
