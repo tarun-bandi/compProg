e49980c54f1f51a994bf7e621bb68e362d617c89
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-19 00:18:31 -0400

    Time: 51 ms (70.15%), Space: 35.7 MB (12.4%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,23 +1,28 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         
-        word_counter = collections.defaultdict(int)
-        palindome_size = 0
-        for word in words:
+        word_ct = collections.Counter(words)
+        found_middle = False
+        result_size = 0
 
-            if word[::-1] in word_counter:
-                word_counter[word[::-1]] -= 1
-
-                if word_counter[word[::-1]] == 0:
-                    del word_counter[word[::-1]]
-                palindome_size += 4
-            else:
-                word_counter[word] += 1
-        
-        for word in word_counter:
+        for word in list(word_ct.keys()):
+            count = word_ct[word]
+  
             if word[0] == word[1]:
-                palindome_size += 2
-                break
-        
-        return palindome_size
+                result_size += (count // 2) * 4
 
+                if count % 2 == 1:
+                    found_middle = True
+            else:
+                reverse = word[::-1]
+                reverse_count = word_ct[reverse] 
+                pair_count = min(reverse_count, count)
+                result_size += pair_count * 4
+                word_ct[reverse] -= pair_count
+                word_ct[word] -= pair_count
+
+        
+        if found_middle:
+            result_size += 2
+        
+        return result_size
