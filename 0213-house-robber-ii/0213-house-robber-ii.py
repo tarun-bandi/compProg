65daa70ff7a88d7702f573a1aafd72aa01cd6360
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-19 00:12:40 -0400

    Time: 0 ms (100%), Space: 17.8 MB (38.48%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,17 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def solve(nums):
+            @cache
+            def rob_from(i: int) -> int:
+                if i >= len(nums):
+                    return 0
+                
+                return max(rob_from(i + 2) + nums[i], rob_from(i + 1))
+            return rob_from(0)
         
-        @cache
-        def rob_from(i: int, robbed_first: bool) -> int:
-            """ Solves problem for nums[i:] """
-            if i >= len(nums):
-                return 0
-            
-            # Make sure circular case is handled
-            if i == len(nums) - 1 and robbed_first:
-                return 0
-            
-            return max(nums[i] + rob_from(i + 2, robbed_first), rob_from(i + 1, robbed_first))
+        return max(solve(nums[1:]), solve(nums[:-1]))
         
-        return max(rob_from(2, True) + nums[0], rob_from(1, False))
-
+        
