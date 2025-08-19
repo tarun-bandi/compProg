8ff5c31d9f76b36c4778dfdbdc96fd38b119ebd6
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-08-19 10:27:02 -0400

    Time: 1172 ms (5.01%), Space: 32.1 MB (25.22%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,22 +1,19 @@
 from sortedcontainers import SortedDict
-
-
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
+        
         window = SortedDict()
         left = 0
-        max_length = 0
-
-        for right in range(len(nums)):
-            window[nums[right]] = window.get(nums[right], 0) + 1
+        longest_window = 0
+        for r in range(len(nums)):
+            window[nums[r]] = window.get(nums[r], 0) + 1
 
             while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
+
                 window[nums[left]] -= 1
                 if window[nums[left]] == 0:
-                    window.pop(nums[left])
-
+                    del window[nums[left]]
                 left += 1
+            longest_window = max(longest_window, r - left + 1)
+        return longest_window
 
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
