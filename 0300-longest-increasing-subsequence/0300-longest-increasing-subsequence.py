5beab2d65d73e0b92890ea08e1d7c2ce5755cb09
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-06 22:35:14 -0400

    Time: 2074 ms (15.65%), Space: 20.5 MB (6.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,13 +1,28 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [0 for i in range(len(nums))]
+        
+        # Subsequences = 2^n 
+        # Greedy or Dynamic Programming approach
 
-        dp[0] = 1
+        # Subproblems: Subsequence ending at index i 
 
-        for i in range(1, len(nums)):
-            maxVal = 0
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    maxVal = max(maxVal, dp[j])
-            dp[i] = maxVal + 1
-        return max(dp)
+        @cache
+        def find_LIS_ending_at(index: int) -> int:
+            if index >= len(nums):
+                return 0
+
+            longest_subsequence_before = 0
+            for candidate in range(0, index):
+                if nums[candidate] < nums[index]:
+                    longest_subsequence_before = max(longest_subsequence_before, find_LIS_ending_at(candidate))
+
+            print(f"for {index}, longest_subseq_before is {longest_subsequence_before}")
+            return 1 + longest_subsequence_before
+
+        longest_subseq = float("-inf")
+
+        # Look through all endpoints
+        for i in range(len(nums)):
+            longest_subseq = max(longest_subseq, find_LIS_ending_at(i))
+
+        return longest_subseq
