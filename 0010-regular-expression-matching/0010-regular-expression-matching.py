a2ef4696e109d20168ecbca692465deeab06d30e
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-07 12:21:15 -0400

    Time: 9 ms (47.18%), Space: 18.1 MB (13.69%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,26 +1,24 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        
         @cache
-        def dp(i, j):
-            print(i, j)
-            if i < 0 and j < 0:
+        def find_matches_from_index(i: int, j: int):
+            
+            if i >= len(s) and j >= len(p):
                 return True
-            if j < 0:
-                return False
-            if i < 0:
-                if p[j] == '*':
-                    return dp(i, j - 2)
+            if j >= len(p):
                 return False
 
-            is_possible = False
-            if s[i] == p[j] or p[j] == '.':
-                is_possible |= dp(i - 1, j - 1)
-            if p[j] == '*':
-                regex_char = p[j - 1]
-                if s[i] == regex_char or regex_char == '.':
-                    is_possible |= dp(i - 1, j)
-                    is_possible |= dp(i - 1, j - 2)
-                is_possible |= dp(i, j - 2)
-            return is_possible
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+
+            if (j + 1) < len(p) and p[j + 1] == '*':
+                dont_use = find_matches_from_index(i, j + 2)
+                use = match and find_matches_from_index(i + 1, j)
+                return use or dont_use
+            
+            if match:
+                return find_matches_from_index(i + 1, j + 1)
+            
+            return False
         
-        return dp(len(s) - 1, len(p) - 1)
+        return find_matches_from_index(0, 0)
