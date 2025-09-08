74691580b2825ce3a59bee8fa00ff8f719bd6508
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-08 13:11:26 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,19 +1,14 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        
-        @cache
-        def recursivelyBreakInteger(n: int) -> int:
-            if n == 1:
-                return 1
-            if n < 2:
-                return 0
-            if n == 2:
-                return 1
-            
-            current_product = 0
-            for i in range(2, n):
-                current_product = max(current_product, i * recursivelyBreakInteger(n - i), i * (n - i))
 
-            return max(current_product, recursivelyBreakInteger(n - 1))
-        
-        return recursivelyBreakInteger(n)
+        def find_largest_product(N: int):
+            if N <= 3:
+                return N
+            largest_prod = 0
+            for i in range(2, N):
+                largest_prod = max(largest_prod, find_largest_product(N - i) * i)
+            return largest_prod
+
+        if n <= 3:
+            return n - 1
+        return find_largest_product(n)
