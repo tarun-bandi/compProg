229d998c9444717e841670222a3ee90b4f3be71a
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-08 15:21:45 -0400

    Time: 0 ms (100%), Space: 17.9 MB (65.68%) - LeetHub

--- 0909-stone-game/0909-stone-game.py
+++ 0909-stone-game/0909-stone-game.py
@@ -1,11 +1,11 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        
-        @cache
-        def dp(i: int, j: int):
-            if i > j:
-                return 0
+        return True
+        # @cache
+        # def dp(i: int, j: int):
+        #     if i > j:
+        #         return 0
             
-            return max(piles[i] - dp(i + 1, j), piles[j] - dp(i, j - 1))
+        #     return max(piles[i] - dp(i + 1, j), piles[j] - dp(i, j - 1))
         
-        return dp(0, len(piles) - 1) > 0
+        # return dp(0, len(piles) - 1) > 0
