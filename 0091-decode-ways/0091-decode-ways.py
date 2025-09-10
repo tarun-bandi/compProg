e2903934cb6b69b184b702c40cc2ef59264c0591
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 15:40:00 -0400

    Time: 0 ms (100%), Space: 17.8 MB (68.68%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,21 +1,22 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        number_of_decodings_from = [0 for _ in range(n + 1)]
-        number_of_decodings_from[n] = 1
-        number_of_decodings_from[n - 1] = 1 if s[n - 1] != '0' else 0
+        number_of_decodings_from2 = 1
+        number_of_decodings_from1 = 1 if s[n - 1] != '0' else 0
 
         for i in range(n - 2, -1, -1):
             if s[i] == '0':
-                number_of_decodings_from[i] = 0
+                number_of_decodings_from2 = number_of_decodings_from1
+                number_of_decodings_from1 = 0
                 continue
 
             next_2 = int(s[i:i + 2])
-            ways = number_of_decodings_from[i + 1]
+            ways = number_of_decodings_from1
             if 10 <= next_2 <= 26:
-                ways += number_of_decodings_from[i + 2]
-            number_of_decodings_from[i] = ways
-        return number_of_decodings_from[0]
+                ways += number_of_decodings_from2
+            number_of_decodings_from2 = number_of_decodings_from1
+            number_of_decodings_from1 = ways
+        return number_of_decodings_from1
             
 
         @cache
