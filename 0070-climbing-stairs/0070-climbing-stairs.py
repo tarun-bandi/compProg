5aee446f4341011116228afd4acbe4483eb7f196
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 15:04:29 -0400

    Time: 0 ms (100%), Space: 17.8 MB (30.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,30 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        """
+        i [0, n]
+
+        Order: term i requires
+                term i - 1, term i - 2
+                0 before n
+        """
+
+            
+
+        count_ways_values = [None] * (n + 1)
+
+        count_ways_values[0] = 1
+        count_ways_values[1] = 1
+        for i in range(2, n + 1):
+            count_ways_values[i] = count_ways_values[i - 1] + count_ways_values[i - 2]
+        return count_ways_values[-1]
+
         @cache
-        def count_ways(n: int):
-            if n == 0:
+        def count_ways(i: int):
+            if i == 0:
                 return 1
-            if n == 1:
+            if i == 1:
                 return 1
 
-            return count_ways(n - 1) + count_ways(n - 2)
+            return count_ways(i - 1) + count_ways(i - 2)
         
         return count_ways(n)
