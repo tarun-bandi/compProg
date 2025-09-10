82bf206d0864aa598eda1d45bb84d1800babfcd7
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 15:27:13 -0400

    Time: 0 ms (100%), Space: 17.7 MB (82.41%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0 for _ in range(n + 2)]
+
+        for i in range(n - 1, -1, -1):
+            take = nums[i] + dp[i + 2]
+            dont_take = dp[i + 1]
+            dp[i] = max(take, dont_take)
+        
+        return dp[0]
         
         @cache
         def dp(i: int):
