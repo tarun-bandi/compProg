67436bf967b269cc6938aa5f40bb93c42a364828
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 23:49:34 -0400

    Time: 5 ms (47.91%), Space: 20.8 MB (13.35%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,13 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        dp1_buy, dp1_sell = 0, 0
-        dp2_buy = 0
+        
+        @cache
+        def max_profit_starting_from(day: int, owned: bool):
+            if day >= len(prices):
+                return 0
 
-        for i in range(n - 1, -1, -1):
-            dp_buy = max(dp1_sell - prices[i], dp1_buy)
-            dp_sell = max(dp2_buy + prices[i], dp1_sell)
-            dp2_buy = dp1_buy
-            dp1_buy, dp1_sell = dp_buy, dp_sell
-
-        return dp1_buy
+            if owned:
+                sell = max_profit_starting_from(day + 2, False) + prices[day]
+                keep = max_profit_starting_from(day + 1, True)
+                return max(sell, keep)
+            else:
+                buy = max_profit_starting_from(day + 1, True) - prices[day]
+                keep = max_profit_starting_from(day + 1, False)
+                return max(buy, keep)
+        
+        return max_profit_starting_from(0, False)
