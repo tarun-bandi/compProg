94200b5ac89635d779cb774019133c4acb7b6ac8
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 23:00:51 -0400

    Time: 4 ms (41.97%), Space: 17.8 MB (87.03%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,5 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = {len(s) : True}
+
+        for i in range(len(s) - 1, -1, -1):
+            
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    if i + len(w) in dp and dp[i + len(w)]:
+                        dp[i] = True
+            if i not in dp:
+                dp[i] = False
+        
+        return dp[0]
+
+
         memo = {len(s) : True}
         def dfs(i):
             if i in memo:
