037fb2ad0e7d9bf5139b86a41e050d830049997d
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 22:56:52 -0400

    Time: 0 ms (100%), Space: 18 MB (45.56%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,36 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        wordDict = set(wordDict)
-        n = len(s)
-        dp = [[0] * (n + 1) for _ in range(n + 1)]
-        dp[n][n] = True
-        for i in range(n):
-            dp[i][n] = False
+        memo = {len(s) : True}
+        def dfs(i):
+            if i in memo:
+                return memo[i]
 
-        for i in range(n - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                take = False
-                curr_string = s[i: j + 1]
-                if curr_string in wordDict:
-                    take = dp[j + 1][j + 1]
-                not_take = dp[i][j + 1]
-                dp[i][j] = take or not_take
-        
-        return dp[0][0]
+            for w in wordDict:
+                if ((i + len(w)) <= len(s) and
+                     s[i : i + len(w)] == w
+                ):
+                    if dfs(i + len(w)):
+                        memo[i] = True
+                        return True
+            memo[i] = False
+            return False
 
-
-
-        @cache
-        def dp(i: int, j: int):
-            if i >= len(s) and j >= len(s):
-                return True
-            if j >= len(s):
-                return False
-            sting = s[i:j + 1]
-            take = False
-            if sting in wordDict:
-                take = dp(j + 1, j + 1)
-            not_take = dp(i, j + 1)
-            return take or not_take
-        
-        return dp(0, 0)
+        return dfs(0)
