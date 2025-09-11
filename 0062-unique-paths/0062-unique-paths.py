9706c892b521a49a4fb3ba6f46d2193929f46169
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 23:42:39 -0400

    Time: 0 ms (100%), Space: 17.7 MB (88.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,6 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
+        paths_from_origin_to = [[1] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(2, m + 1):
+            for j in range(2, n + 1):
+                paths_from_origin_to[i][j] = paths_from_origin_to[i - 1][j] + paths_from_origin_to[i][j - 1]
+        
+        return paths_from_origin_to[m][n]
         @cache
         def find_path_from_origin_to(x: int, y: int) -> int:
             if x == 1 or y == 1: 
