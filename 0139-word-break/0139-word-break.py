fa079d2efd22b5f2bb3bc2f9d9813d825822b56b
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-10 22:52:02 -0400

    Time: 15 ms (5.13%), Space: 18.2 MB (8.69%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,16 +1,34 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
         wordDict = set(wordDict)
+        n = len(s)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        dp[n][n] = True
+        for i in range(n):
+            dp[i][n] = False
+
+        for i in range(n - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                take = False
+                curr_string = s[i: j + 1]
+                if curr_string in wordDict:
+                    take = dp[j + 1][j + 1]
+                not_take = dp[i][j + 1]
+                dp[i][j] = take or not_take
+        
+        return dp[0][0]
+
+
+
         @cache
         def dp(i: int, j: int):
             if i >= len(s) and j >= len(s):
                 return True
             if j >= len(s):
                 return False
-            strng = s[i:j + 1]
+            sting = s[i:j + 1]
             take = False
-            if strng in wordDict:
+            if sting in wordDict:
                 take = dp(j + 1, j + 1)
             not_take = dp(i, j + 1)
             return take or not_take
