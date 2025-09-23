e8cc94b43b6cccdfc4889aaee7005c3ac78d07e5
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-23 13:36:02 -0400

    Time: 4 ms (71.67%), Space: 38.3 MB (83.94%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> freq;
-        for (char c : tasks)
-            freq[c]++;
-
-        // max-heap based on remaining frequency
-        priority_queue<int> pq;
-        for (auto& it : freq)
-            pq.push(it.second);
-
-        int time = 0;
-        while (!pq.empty()) {
-            int cycle = n + 1;  
-            vector<int> tmp;
-
-            // Try to execute up to (n+1) tasks in one cycle
-            while (cycle > 0 && !pq.empty()) {
-                int cnt = pq.top(); pq.pop();
-                if (--cnt > 0) tmp.push_back(cnt);
-                time++;
-                cycle--;
-            }
-
-            // Push remaining tasks back
-            for (int cnt : tmp) pq.push(cnt);
-
-            // If pq is not empty, we need to account for idle time
-            if (!pq.empty()) time += cycle;
+        // Count frequency of each task
+        unordered_map<char, int> taskCounts;
+        for (char task : tasks) {
+            taskCounts[task]++;
         }
-
-        return time;
+        
+        // Find the maximum frequency
+        int maxFreq = 0;
+        for (auto& pair : taskCounts) {
+            maxFreq = max(maxFreq, pair.second);
+        }
+        
+        // Count how many tasks have the maximum frequency
+        int maxFreqCount = 0;
+        for (auto& pair : taskCounts) {
+            if (pair.second == maxFreq) {
+                maxFreqCount++;
+            }
+        }
+        
+        // Calculate minimum intervals needed
+        // Framework: (maxFreq - 1) complete cycles, each needs (n + 1) slots
+        // Plus final execution of all max frequency tasks
+        int frameworkTime = (maxFreq - 1) * (n + 1) + maxFreqCount;
+        
+        // Result is at least the total number of tasks
+        return max((int)tasks.size(), frameworkTime);
     }
 };
