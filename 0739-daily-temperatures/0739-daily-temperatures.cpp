57d6596926eb0586fe72bc26d61d61cf32e4926f
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-24 11:41:47 -0400

    Time: 34 ms (17.58%), Space: 112 MB (5.34%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         stack<pair<int, int>> temp_and_idx;
-        vector<int> result_temperatures(temperatures.size());
+        vector<int> days_to_warmer(temperatures.size());
 
         for (int i = 0; i < temperatures.size(); ++i){
             int todays_temp = temperatures[i];
             
             while (!temp_and_idx.empty() && temp_and_idx.top().first < todays_temp){
                 int colder_index = temp_and_idx.top().second;
-                result_temperatures[colder_index] = i - colder_index;
+                days_to_warmer[colder_index] = i - colder_index;
                 temp_and_idx.pop();
             }
             temp_and_idx.push({todays_temp, i});
         }
-        
-        // while (!temp_and_idx.empty()){
-        //     int index = temp_and_idx.pop().second;
-        // }
 
-        return result_temperatures;
+
+        return days_to_warmer;
     }
 };
