678b6d111587ebbfa6df331698bfcbfcb3d4d91a
Tarun Bandi <57272137+tarun-bandi@users.noreply.github.com>
2025-09-26 12:56:09 -0400

    Time: 2 ms (62.03%), Space: 9.6 MB (28.47%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     bool isMatch(string s, string p) {
-        
-   std::function<bool(int, int)> find_matches = [&](int i, int j) -> bool {
-        if (j >= p.size()) return i >= s.size();
-        
-        bool first_match = (i < s.size()) && 
-                          (p[j] == s[i] || p[j] == '.');
-        
-        if (j < p.size() - 1 && p[j + 1] == '*') {
-            return find_matches(i, j + 2) || 
-                   (first_match && find_matches(i + 1, j));
-        } else {
-            return first_match && find_matches(i + 1, j + 1);
+        dp.assign(s.size() + 1, vector<int>(p.size() + 1, -1));
+        return find_match_from(0, 0, s, p);
+    }
+
+    bool find_match_from(int i, int j, string& s, string& p){
+        if (i >= s.size() && j >= p.size()) return true;
+        if (j >= p.size()) return false;
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-    };
 
-    return find_matches(0, 0);
+        bool first_match = i < s.size() && (p[j] == '.' || s[i] == p[j]);
 
-
+        if (j < p.size() - 1 && p[j + 1] == '*') {
+            bool take = first_match && find_match_from(i + 1, j, s, p);
+            bool dont_take = find_match_from(i, j + 2, s, p);
+            dp[i][j] = take || dont_take;
+            return take || dont_take;
+        }
+        dp[i][j] = first_match && find_match_from(i + 1, j + 1, s, p);
+        return dp[i][j];
     }
 };
